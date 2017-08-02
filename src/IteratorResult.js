30ef3adbf709056372709d4f8f50fe5b3774087a
Raoul v. R <vanruesc@outlook.de>
2017-08-02 16:05:09 +0200

    Renamed index to IteratorResult.
    Re-exported in index.js.

---
+++ src/IteratorResult.js
@@ -0,0 +1,50 @@
+/**
+ * A basic iterator result.
+ *
+ * The next method of an iterator always has to return an object with
+ * appropriate properties including done and value.
+ */
+
+export default class IteratorResult {
+
+	/**
+	 * Constructs a new iterator result.
+	 *
+	 * @param {Vector3} [value=null] - A value.
+	 * @param {Vector3} [done=false] - Whether this result is past the end of the iterated sequence.
+	 */
+
+	constructor(value = null, done = false) {
+
+		/**
+		 * An arbitrary value returned by the iterator.
+		 *
+		 * @type Object
+		 * @default null
+		 */
+
+		this.value = value;
+
+		/**
+		 * Whether this result is past the end of the iterated sequence.
+		 *
+		 * @type Boolean
+		 * @default false
+		 */
+
+		this.done = done;
+
+	}
+
+	/**
+	 * Resets this iterator result.
+	 */
+
+	reset() {
+
+		this.value = null;
+		this.done = false;
+
+	}
+
+}
