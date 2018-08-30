0d4de2770d867a5c3cd35327d44a5cf22c5b129b
Raoul v. R <vanruesc@users.noreply.github.com>
2018-08-31 00:13:00 +0200

    Fresh build.

--- build/iterator-result.js
+++ build/iterator-result.js
@@ -1,5 +1,5 @@
 /**
- * iterator-result v0.0.3 build Mon Jul 02 2018
+ * iterator-result v0.0.3 build Fri Aug 31 2018
  * https://github.com/vanruesc/iterator-result
  * Copyright 2018 Raoul van Rüschen, Zlib
  */
@@ -9,51 +9,48 @@
   (global.ITERATORRESULT = factory());
 }(this, (function () { 'use strict';
 
-  var classCallCheck = function (instance, Constructor) {
+  function _classCallCheck(instance, Constructor) {
     if (!(instance instanceof Constructor)) {
       throw new TypeError("Cannot call a class as a function");
     }
-  };
+  }
 
-  var createClass = function () {
-    function defineProperties(target, props) {
-      for (var i = 0; i < props.length; i++) {
-        var descriptor = props[i];
-        descriptor.enumerable = descriptor.enumerable || false;
-        descriptor.configurable = true;
-        if ("value" in descriptor) descriptor.writable = true;
-        Object.defineProperty(target, descriptor.key, descriptor);
-      }
+  function _defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
     }
+  }
 
-    return function (Constructor, protoProps, staticProps) {
-      if (protoProps) defineProperties(Constructor.prototype, protoProps);
-      if (staticProps) defineProperties(Constructor, staticProps);
-      return Constructor;
-    };
-  }();
+  function _createClass(Constructor, protoProps, staticProps) {
+    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) _defineProperties(Constructor, staticProps);
+    return Constructor;
+  }
 
   var IteratorResult = function () {
-  	function IteratorResult() {
-  		var value = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
-  		var done = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
-  		classCallCheck(this, IteratorResult);
+    function IteratorResult() {
+      var value = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
+      var done = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
 
+      _classCallCheck(this, IteratorResult);
 
-  		this.value = value;
+      this.value = value;
+      this.done = done;
+    }
 
-  		this.done = done;
-  	}
+    _createClass(IteratorResult, [{
+      key: "reset",
+      value: function reset() {
+        this.value = null;
+        this.done = false;
+      }
+    }]);
 
-  	createClass(IteratorResult, [{
-  		key: "reset",
-  		value: function reset() {
-
-  			this.value = null;
-  			this.done = false;
-  		}
-  	}]);
-  	return IteratorResult;
+    return IteratorResult;
   }();
 
   return IteratorResult;
