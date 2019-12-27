901ec0d256307ae4b716a00e5e5913fb2cefb01b
Raoul v. R <vanruesc@users.noreply.github.com>
2019-12-27 21:44:08 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import babel from "rollup-plugin-babel";
 import minify from "rollup-plugin-babel-minify";
-import resolve from "rollup-plugin-node-resolve";
 
 const pkg = require("./package.json");
 const date = (new Date()).toDateString();
@@ -11,33 +10,50 @@ const banner = `/**
  * Copyright ${date.slice(-4)} ${pkg.author.name}, ${pkg.license}
  */`;
 
+const production = (process.env.NODE_ENV === "production");
+
 const lib = {
 
-	input: pkg.module,
-	output: {
-		file: "build/" + pkg.name + ".js",
-		format: "umd",
-		name: pkg.name.replace(/-/g, "").toUpperCase(),
-		banner: banner
+	module: {
+		input: "src/index.js",
+		output: [{
+			file: pkg.module,
+			format: "esm",
+			banner
+		}, {
+			file: pkg.main,
+			format: "esm"
+		}, {
+			file: pkg.main.replace(".js", ".min.js"),
+			format: "esm"
+		}]
 	},
 
-	plugins: [resolve()].concat(process.env.NODE_ENV === "production" ? [babel()] : [])
+	main: {
+		input: pkg.main,
+		plugins: [babel()],
+		output: {
+			file: pkg.main,
+			format: "umd",
+			name: pkg.name.replace(/-/g, "").toUpperCase(),
+			banner
+		}
+	},
+
+	min: {
+		input: pkg.main.replace(".js", ".min.js"),
+		plugins: [minify({
+			bannerNewLine: true,
+			comments: false
+		}), babel()],
+		output: {
+			file: pkg.main.replace(".js", ".min.js"),
+			format: "umd",
+			name: pkg.name.replace(/-/g, "").toUpperCase(),
+			banner
+		}
+	}
 
 };
 
-export default [lib].concat((process.env.NODE_ENV === "production") ? [
-
-	Object.assign({}, lib, {
-
-		output: Object.assign({}, lib.output, {
-			file: "build/" + pkg.name + ".min.js"
-		}),
-
-		plugins: [resolve(), babel(), minify({
-			bannerNewLine: true,
-			comments: false
-		})]
-
-	})
-
-] : []);
+export default [lib.module, lib.main, lib.min];
